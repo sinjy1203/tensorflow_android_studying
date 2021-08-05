0b9bc75814725303c7fb1b462481bef6e1d71b19
sinjy <sinjy1203@gmail.com>
2021-08-05 17:04:38 +0900

    commit

--- DigitClassifier_app/python/tflite_model.py
+++ DigitClassifier_app/python/tflite_model.py
@@ -20,6 +20,9 @@ image = keras.preprocessing.image.load_img(
 )
 
 ##
+input_image = (np.array(image, dtype=np.float32) / 255.)[np.newaxis, ..., np.newaxis]
+
+##
 show_image(image, label=0)
 
 ##
@@ -28,4 +31,17 @@ with open(tflite_path, 'rb') as f:
     tflite_model = f.read()
 
 ##
+interpreter = tf.lite.Interpreter(model_content=tflite_model)
+interpreter.allocate_tensors()
 
+##
+interpreter.set_tensor(interpreter.get_input_details()[0]["index"], input_image)
+interpreter.invoke()
+output = interpreter.tensor(interpreter.get_output_details()[0]['index'])()[0]
+
+##
+pred_digit = np.argmax(output)
+
+##
+show_image(image, pred_digit)
+print("confidence: {}".format(output[pred_digit]))
